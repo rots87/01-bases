58867f03ec26a68c38c5012551f532f9e3b98214
Nestor Cañas <ncanas87@gmail.com>
2021-10-08 17:00:54 -0600

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css'],
+})
+export class ListadoComponent {
+  heroes: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Capitán América',
+  ];
+
+  heroeBorrado: string = '';
+
+  borrando(): void {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
